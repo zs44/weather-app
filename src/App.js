053b2e0e67777c76a2ddd07116f8ee9e6609e1e9
z-samadi <samadi83@gmail.com>
2023-08-02 17:25:05 +0330

    remove extra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Weather city="london" />
       </header>
+      <footer>
+        This project was coded by Zeinab and is <a href="" target="_blank">open source on GitHub</a>
+      </footer>
     </div>
   );
 }
