9ec19ede48405ca8a661f2198641d2993aa0df82
z-samadi <samadi83@gmail.com>
2023-08-02 17:47:49 +0330

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather city="london" />
-      </header>
-      <footer>
-        This project was coded by Zeinab and is <a href="" target="_blank">open source on GitHub</a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <Weather city="london" />
+        </header>
+        <footer>
+          This project was coded by Zeinab and is{" "}
+          <a href="https://github.com/zs44/weather-app" target="_blank">
+            open source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
