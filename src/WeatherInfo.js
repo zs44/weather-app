910d8268590007e3ac0d6a8a5808436df1822195
z-samadi <samadi83@gmail.com>
2023-08-04 12:23:16 +0330

    fix css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,11 +14,11 @@ export default function WeatherInfo(props) {
       </ul>
 
       <div className="row">
-        <div className="col-6">
+        <div className="col-6 d-flex new">
           <WeatherIcon code={props.info.icon} size={52} />
           <WeatherTemprature Celsious={props.info.temprature} />
         </div>
-        <div className="col-6">
+        <div className="col-6 new">
           <ul>
             <li>Humidity: {props.info.humidity}%</li>
             <li>Wind: {props.info.wind} km/h</li>
