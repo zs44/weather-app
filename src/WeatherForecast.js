82879c5d0643fdc4fd30a17f4935fcc21611959c
z-samadi <samadi83@gmail.com>
2023-08-04 22:16:50 +0330

    update forecast cord

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
@@ -13,6 +13,9 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+useEffect(() => {
+  setLoaded(false);
+}, [props.coords]);
 
   
   if (loaded) { 
